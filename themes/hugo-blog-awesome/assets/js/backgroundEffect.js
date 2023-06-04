3a50c856182977d41c6c746a7e5ee5771e658010
Michael Dodis <michaeldodisgr@gmail.com>
2023-06-04 12:40:58 +0300

    disable effect on mobile

--- themes/hugo-blog-awesome/assets/js/backgroundEffect.js
+++ themes/hugo-blog-awesome/assets/js/backgroundEffect.js
@@ -322,6 +322,16 @@ function addBlobs() {
 }
 
 function main() {
+    if (navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/iPad/i)
+        || navigator.userAgent.match(/iPod/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i)) {
+        return;
+    }
+
     canvas = document.getElementById('bg-canvas');
     gl = canvas.getContext('webgl2', { premultipliedAlpha: false });
     console.log(`Width: ${canvas.width}, Height: ${canvas.height}`);
